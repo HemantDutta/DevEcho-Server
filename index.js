03e75f0457a4dfadf5ffc292cd7edbbf0c996d18
HemantDutta <hemantnorddutta2@gmail.com>
2023-12-04 20:07:28 +0530

    Create Supabase Client | Configure env

--- index.js
+++ index.js
@@ -19,14 +19,28 @@ app.use(function (req, res, next) {
     next();
 });
 
+//Supabase Client
+const supabase = createClient(process.env.SUPA_URL, process.env.SUPR_KEY);
+
 //Request Handlers
 
 //Default URL
-app.get("/", (req,res)=>{
-   res.send(`<h1>DevEcho Server</h1>`);
+app.get("/", (req, res) => {
+    res.send(`<h1>DevEcho Server</h1>`);
+});
+
+//Fetch Posts
+app.get("/fetch-posts", async (req, res) => {
+    const {data, error} = await supabase
+        .from("posts")
+        .select();
+
+    if (error) res.send("error");
+    if (data[0]) res.send(data);
+    else res.send("no_data");
 });
 
 //Server Listener
 app.listen(port, () => {
-   console.log("The server is listening on http://localhost:"+port);
+    console.log("The server is listening on http://localhost:" + port);
 });
